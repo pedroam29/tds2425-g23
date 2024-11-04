e501771298015003fa605dc6d0ad7eebba0d6d96
Pedro Arias Montes <pedro.ariasm@um.es>
2024-11-04 11:57:21 +0100

    Clase Apphat

--- MavenTestProject/src/main/java/tds/appchat/controlador/AppChat.java
+++ MavenTestProject/src/main/java/tds/appchat/controlador/AppChat.java
@@ -3,9 +3,11 @@ package tds.appchat.controlador;
 import java.util.List;
 
 import tds.appchat.modelo.Mensaje;
+import tds.appchat.modelo.RepositorioUsuarios;
 import tds.appchat.modelo.Usuario;
 
 public class AppChat {
 	private Usuario usuarioActual;
+	private RepositorioUsuarios repoUsuarios;
 	
 }
